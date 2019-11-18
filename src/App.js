f1286404c71e1f1bf98884bab922be62c2ace58a
Tim Parsons <timmparsons85@gmail.com>
2019-11-18 06:48:16 -0700

    added data to app component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React from 'react';
 import './App.css';
 import ApiCallContainer from './components/ApiCallContainer'
 
-function App() {
-  return (
-    <div>
-      <h1>PeoplesBi</h1>
-      <ApiCallContainer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state ={
+      data : [ 
+        {
+          name: "Dave",
+          location: "Arizona"
+        },
+        {
+          name: "John",
+          location: "California"
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>PeoplesBi</h1>
+        <ApiCallContainer data={this.state.data}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ApiCallContainer.js
+++ src/components/ApiCallContainer.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ApiCall from './ApiCall';
 
-const ApiCallContainer = () => (
- <ApiCall />
-)
+const ApiCallContainer = (props) => {
+  return (
+    <div>{props.name}</div>
+  )
+}
 
 export default ApiCallContainer;
